d320df9b070475378f5637d87743d8e277f2fd7b
deeper1007 <145514209+deeper1007@users.noreply.github.com>
2024-11-17 18:25:51 -0500

    Add files via upload

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+import com.soundicly.jnanoidenhanced.jnanoid.NanoIdUtils;
+public class Main {
+    public static void main(String[] args) {
+
+        String id = NanoIdUtils.randomNanoId();
+        System.out.println("Hello world!"+id);
+    }
+}
